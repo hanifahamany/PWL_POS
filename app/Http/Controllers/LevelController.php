61f38002a9b1d8bb9b8134b951fe0a94028e3b16
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-03-26 19:44:37 +0700

    P 4 Jobsheet 3

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -20,7 +20,7 @@ class LevelController extends Controller
         // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
         // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row. ' baris';
 
-        $data = DB::select('slect * from m_level');
+        $data = DB::select('select * from m_level');
         return view('level', ['data' => $data]);
 
     }
