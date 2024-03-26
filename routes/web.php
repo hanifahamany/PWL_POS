d4805bf6effba4f3e185ce611bcf305aa75159dc
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-03-26 19:50:58 +0700

    P 5 Jobsheet 3

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
