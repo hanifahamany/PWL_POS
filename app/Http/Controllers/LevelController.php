ab3c6c72ac940d3822fa5a0b4015ebc07c3f4d02
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-03-26 09:33:19 +0700

    P 3 Jobsheet 3

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,7 +9,7 @@ class LevelController extends Controller
 {
     public function index()
     {
-        DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', nom()]);
+        DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
 
         return 'Insert data baru berhasil';
     }
