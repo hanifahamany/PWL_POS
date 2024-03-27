9c1b670afcf5f167a2bb08756fa52bbd81ece810
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-03-27 21:22:48 +0700

    P 6 Jobsheet 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,14 +12,14 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data =[
-            'usernam' => 'customer-1',
+            'username' => 'customer-1',
             'nama' => 'Pelanggan',
             'password' => Hash::make('12345'),
             'level_id' => 4
 
         ];
         UserModel::insert($data); // tambahkan data ke tabel m_user
-        
+
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
