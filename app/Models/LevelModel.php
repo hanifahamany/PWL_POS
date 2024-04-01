8cf0980f6f1290edd50c2faf9d923551043f08f0
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-04-01 14:17:21 +0700

    P 2.7 Jobsheet 4

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -9,15 +9,10 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class LevelModel extends Model
 {
     use HasFactory;
-
-    protected $table = 'levels'; // sesuaikan dengan nama tabel yang digunakan
-
-    protected $primaryKey = 'id'; // sesuaikan dengan primary key tabel
-
-    protected $fillable = ['name']; // kolom yang dapat diisi secara massal
-
-    public function users(): HasMany
-    {
-        return $this->hasMany(UserModel::class);
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+    protected $fillable = ['level_kode','level_nama'];
+    public function users(): HasMany{
+        return $this->hasMany(User::class);
     }
 }
