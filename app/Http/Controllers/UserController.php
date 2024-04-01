8c2b9fc30db95216c618f50cafbb20e966541e26
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-04-01 12:07:31 +0700

    P 2.3 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,6 +40,7 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(3);
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         $user = UserModel::where('level_id', 2)->count();
+        dd($user);
         return view('user', ['data' => $user]);
     }
 }
