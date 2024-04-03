e754c2f3f68a39749efa3cb8f3f31dacab638774
Hanifah Amany <2241720127@student.polinema.ac.id>
2024-04-03 21:57:44 +0700

    P 2 Jobsheet 5

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,6 +7,8 @@
     @hasSection('subtitle') | @yield('subtitle') @endif
 @stop
 
+@vite('resources/js/app.js')
+
 {{-- Extend and customize the page content header --}}
 
 @section('content_header')
@@ -47,18 +49,16 @@
 {{-- Add common Javascript/Jquery code --}}
 
 @push('js')
-<script>
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
 
-    $(document).ready(function() {
-        // Add your common script logic here...
-    });
-
-</script>
 @endpush
+
+@stack('scripts')
 
 {{-- Add common CSS customizations --}}
 
 @push('css')
+<link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />
 <style type="text/css">
 
     {{-- You can add AdminLTE customizations here --}}
